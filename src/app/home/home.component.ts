90000097581cba64000f7f23efa2cafc5f7cd122
David Yepes <djvash01@gmail.com>
2023-07-29 17:35:46 -0500

    feat: 🎸 read data mock with job list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Job } from '@models/job.model';
+import { RequestService } from '@services/request/request.service';
 import { ChipType } from '@shared/chip/chip-type.enum';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +13,11 @@ export class HomeComponent {
 
   public square = ChipType.SQUARE;
   public rounded = ChipType.ROUNDED;
+  private urlData = 'assets/mocks/data.json';
+  public jobs: Observable<Job[]>;
+
+  constructor(private readonly request: RequestService){
+    this.jobs = this.request.get<Job[]>(this.urlData);
+  }
 
 }
